b8f1824151fa5001563093b2eb26dadc116c2bd5
James C <jcottrelliii@gmail.com>
2023-10-10 15:47:27 -0500

    interger project

---
+++ main.py
@@ -0,0 +1,9 @@
+# Convert input to integer
+current_time = int(input("What is the current time (in hours)?"))
+# Convert input to integer
+wait_time = int(input("How many hours do you want to wait"))
+
+print(current_time)
+print(wait_time)
+final_time = current_time + wait_time
+print(final_time)
